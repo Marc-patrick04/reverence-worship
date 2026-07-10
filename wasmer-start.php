5b61a5b25e72f0f0d8a4a4615daefc1f66665350
Marc <marcpatrick04@gmail.com>
2026-07-10 14:51:58 +0200

    start up testing

--- wasmer-start.php
+++ wasmer-start.php
@@ -18,5 +18,19 @@ foreach ($dirs as $dir) {
     }
 }
 
-passthru('php -t /app/public -S localhost:8080 /app/public/index.php', $exitCode);
+$router = '/app/wasmer-router.php';
+file_put_contents($router, <<<'PHP'
+<?php
+
+$path = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH) ?: '/';
+$file = realpath('/app/public' . $path);
+
+if ($file !== false && str_starts_with($file, '/app/public') && is_file($file)) {
+    return false;
+}
+
+require '/app/public/index.php';
+PHP);
+
+passthru('php -t /app/public -S localhost:8080 ' . escapeshellarg($router), $exitCode);
 exit($exitCode);
