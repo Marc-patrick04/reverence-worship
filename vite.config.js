e5a965a8042f4e951dd9d60510c23aeeb13e8b4f
Marc <marcpatrick04@gmail.com>
2026-07-10 17:00:57 +0200

    rwanda geo

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/locations.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
@@ -15,9 +15,4 @@ export default defineConfig({
             ignored: ['**/storage/framework/views/**'],
         },
     },
-    resolve: {
-        alias: {
-            '@devrw/rwanda-location': 'node_modules/@devrw/rwanda-location/dist/index.esm.js'
-        }
-    }
 });
